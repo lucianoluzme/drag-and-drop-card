7b34906ce0c9f0a215184dcaa661b24af0228c13
lucianoluzme <lucianno.contato@gmail.com>
2022-06-19 13:08:22 -0300

    19062022

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-
+/*Drag*/
 const dropItems = document.getElementById('drop-items')
 
 new Sortable(dropItems, {
@@ -6,3 +6,24 @@ new Sortable(dropItems, {
     chosenClass: 'sortable-chosen',
     dragClass: 'sortable-drag'
 });
+
+ /*Show Modal*/
+ const showModal = (openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+
+    if(openBtn && modalContainer) {
+        openBtn.addEventListener('click', ()=>{
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+
+showModal('open-modal', 'modal-container')
+
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
